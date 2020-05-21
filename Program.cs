a6677908e2a706545005141b157fc3f9b83b9a5c
fun4wut <nihouze@163.com>
2020-05-21 15:34:18 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace img_search
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        static void Main(string[] args)
+        {
+            int a = 2, b = 3;
+            Swap(ref a, ref b);
+            Console.WriteLine(a - b);
+        }
+    }
+}
